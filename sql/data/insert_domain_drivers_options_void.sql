75a890f336a77d681394b287fa759ae166ecb0fc
Francesc Guasch <frankie@telecos.upc.edu>
2017-09-15 07:19:53 +0200

    [#222] void drivers for testing

---
+++ sql/data/insert_domain_drivers_options_void.sql
@@ -0,0 +1,2 @@
+INSERT INTO `domain_drivers_options` VALUES (100,100,'QXL','value=qxl');
+INSERT INTO `domain_drivers_options` VALUES (101,100,'EGA','value=ega');
---
+++ sql/data/insert_domain_drivers_types_void.sql
@@ -0,0 +1,1 @@
+INSERT INTO `domain_drivers_types` VALUES (100,'video','Video Card','Void');
